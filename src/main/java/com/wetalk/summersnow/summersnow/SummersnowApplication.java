a58132ea45c9c5dcbc38782288c04a27f62df138
tianjay <18048718948@163.com>
2020-04-29 23:57:36 +0800

    git inint repository

---
+++ src/main/java/com/wetalk/summersnow/summersnow/SummersnowApplication.java
@@ -0,0 +1,13 @@
+package com.wetalk.summersnow.summersnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummersnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummersnowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wetalk/summersnow/summersnow/SummersnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wetalk.summersnow.summersnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummersnowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
